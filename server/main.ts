bca02157e9f6ac81d678669a9ae6c484126ecf1b
Eder Almeida <reikron8@gmail.com>
2020-11-12 19:59:32 +0100

    nest server, hello world ajax request

---
+++ server/main.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import { join } from 'path';
+
+const DIST = 'twitter-clone';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    //logger: ['error', 'warn'],
+  });
+  const env = process.env.NODE_ENV || "production";
+  const port = parseInt(process.env.PORT || '3000');
+
+  app.set('env', env);
+  app.set('port', port);
+  app.disable('x-powered-by');
+
+  if (env == "production") {
+    app.useStaticAssets(join(__dirname, '..', DIST));
+    app.setViewEngine('html');
+    app.use((req, res, next) => {
+      if (req.url.startsWith('/api/')) {
+        next();
+      } else {
+        return res.status(200).sendFile('/', { root: join(__dirname, '..', DIST) });
+      }
+    });
+  }
+
+  app.setGlobalPrefix('api');
+  await app.listen(port);
+}
+
+bootstrap();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'twitter-clone';
+  title = 'Twitter Clone';
+  message = "";
+
+  constructor(
+    private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<string>("/api/", { responseType: 'text' as 'json' }).subscribe(message => console.log((this.message = message)));
+  }
 }
